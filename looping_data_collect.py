342b182b030332afd8bde4cc863684f09a574867
Tom Walton <tomwalton78@googlemail.com>
2018-08-12 21:34:48 +0100

    Modify data collecting for multiple currencies

--- looping_data_collect.py
+++ looping_data_collect.py
@@ -3,17 +3,20 @@ from twisted.internet import task, reactor
 from gdax_exchange import GdaxExchange
 from kraken_exchange import KrakenExchange
 
-TIME_BETWEEN_CALLS = 5.0  # secs
+TIME_BETWEEN_CALLS = 10.0  # secs
 
+markets = ['ETH-EUR', 'LTC-EUR', 'BTC-EUR']
 
-g_etheur = GdaxExchange('ETH-EUR')
-g_etheur_LoopingCall = task.LoopingCall(
-    g_etheur.fetch_l1_quote_and_write_to_csv
-).start(TIME_BETWEEN_CALLS)
+for market in markets:
 
-k_etheur = KrakenExchange('ETH-EUR')
-k_etheur_LoopingCall = task.LoopingCall(
-    k_etheur.fetch_l1_quote_and_write_to_csv
-).start(TIME_BETWEEN_CALLS)
+    g_etheur = GdaxExchange(market)
+    g_etheur_LoopingCall = task.LoopingCall(
+        g_etheur.fetch_l1_quote_and_write_to_csv
+    ).start(TIME_BETWEEN_CALLS)
+
+    k_etheur = KrakenExchange(market)
+    k_etheur_LoopingCall = task.LoopingCall(
+        k_etheur.fetch_l1_quote_and_write_to_csv
+    ).start(TIME_BETWEEN_CALLS)
 
 reactor.run()
