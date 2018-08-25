c83d548aca33408e0c6246aa5beca8ef51593426
Tom Walton <tomwalton78@googlemail.com>
2018-08-25 16:40:48 +0100

    Update looping data collection script
    Update looping data collection script to not collect for markets that
    are not available on the specified exchange. Neaten script, adding looping
    through all markets and exchanges specified.

--- looping_data_collect.py
+++ looping_data_collect.py
@@ -1,5 +1,6 @@
 from twisted.internet import task, reactor
 
+from exchange import ExchangeAPICallFailedException
 from gdax_exchange import GdaxExchange
 from kraken_exchange import KrakenExchange
 from bitstamp_exchange import BitstampExchange
@@ -7,30 +8,32 @@ from bitfinex_exchange import BitfinexExchange
 
 TIME_BETWEEN_CALLS = 10.0  # secs
 
+# Specify markets and exchanges to collect data for
 markets = ['ETH-EUR', 'LTC-EUR', 'BTC-EUR']
+exchanges = [GdaxExchange, KrakenExchange, BitstampExchange, BitfinexExchange]
 
+# Check markets exist
+valid_market_exchange_objects = []
 for market in markets:
+    for exchange_obj in exchanges:
 
-    g_etheur = GdaxExchange(market)
-    g_etheur_LoopingCall = task.LoopingCall(
-        g_etheur.fetch_l1_quote_and_write_to_csv
+        try:
+            ex = exchange_obj(market)
+            ex.fetch_l1_quote()
+            valid_market_exchange_objects.append(ex)
+        except ExchangeAPICallFailedException:
+            print(
+                'API call for {} market on the {} exchange failed. \
+                Not collecting.\n'.format(
+                    market, ex.exchange_name
+                ).replace('  ', ''))
+
+# Set up LoopingCall for validated market-exchange objects
+for obj in valid_market_exchange_objects:
+
+    task.LoopingCall(
+        obj.fetch_l1_quote_and_write_to_csv
     ).start(TIME_BETWEEN_CALLS)
 
-    k_etheur = KrakenExchange(market)
-    k_etheur_LoopingCall = task.LoopingCall(
-        k_etheur.fetch_l1_quote_and_write_to_csv
-    ).start(TIME_BETWEEN_CALLS)
-
-    b_etheur = BitstampExchange(market)
-    b_etheur_LoopingCall = task.LoopingCall(
-        b_etheur.fetch_l1_quote_and_write_to_csv
-    ).start(TIME_BETWEEN_CALLS)
-
-    if market != 'LTC-EUR':
-        # TEMPORARY FIX!
-        bf_etheur = BitfinexExchange(market)
-        bf_etheur_LoopingCall = task.LoopingCall(
-            bf_etheur.fetch_l1_quote_and_write_to_csv
-        ).start(TIME_BETWEEN_CALLS)
 
 reactor.run()
